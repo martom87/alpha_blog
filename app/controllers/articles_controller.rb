08eff03a64adb928d922e4b470cf4f46ca9f23f6
martom87 <marcintomala87@gmail.com>
2018-12-19 23:40:04 +0100

    before action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
     @article = Article.new
   end
@@ -22,7 +25,6 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "The article was successfully updated"
       redirect_to article_path(@article)
@@ -32,13 +34,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "The article was destroyed"
     redirect_to articles_path
   end
 
   def show
+  end
+
+  private
+
+  def set_article
     @article = Article.find(params[:id])
   end
 
