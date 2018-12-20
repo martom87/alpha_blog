14054ff0858a049ce456cd54baf66dbcc8e242de
martom87 <marcintomala87@gmail.com>
2018-12-20 16:37:50 +0100

    jquery added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require activestorage
+//= require jquery3
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
