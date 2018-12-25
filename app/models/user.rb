529c5b6900e6a1d13eb5963122d563c7c1283301
martom87 <marcintomala87@gmail.com>
2018-12-25 22:59:44 +0100

    users validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+            length: {maximum: 3, minimum: 25}
+  EMAIL_IS_VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false}, format: {with: EMAIL_IS_VALID_REGEX}
 end
